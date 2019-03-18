350a122782e055848ead0282f4dcb3708c023c97
vishalvora <vishalgvora@gmail.com>
2019-03-18 10:20:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,21 @@ app.get('/', function (req, res) {
 app.listen(app.get('port'), function () {
   console.log('new App is listening on port ' + app.get('port'));
 });
+
+
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://broker.hivemq.com')
+ 
+client.on('connect', function () {
+  client.subscribe('presence', function (err) {
+    if (!err) {
+      client.publish('presence', 'Hello mqtt')
+    }
+  })
+})
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
+})
